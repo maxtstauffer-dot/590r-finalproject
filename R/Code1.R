c6b0cf248d1c364b6682989205f9d6ce8263b5cd
Max Stauffer <maxtstauffer@gmail.com>
2025-08-26 16:22:56 -0400

    Changed csv to xlsx because csv was getting rid of my data dictionary. I also started the R-code and have a working tsummary, after loading in the data and mutating it.

--- R/Code1.R
+++ R/Code1.R
@@ -1,11 +1,31 @@
 
+#Data cleaned using Excel
+
+#Load in proper packages
 
 
+#Making "Here" Package work
+setwd("data")
+
+#Load in the correct data
+library(readxl)
+nlsy_short <- read_excel(here::here("data", "cleaned_internet_midwest_data.xlsx"))
 
 
+# Split by space and unnest
 
+library(tidyverse)
+nlsy <- nlsy_short %>%
+	mutate(midwest_states = str_split(midwest_states, " ")) %>%
+	unnest(midwest_states)
 
-
-
+#Create a {gtsummary} table of descriptive statistics about your data (1 pt)
+library(gtsummary)
+tbl_summary(
+	nlsy,
+	by = midwest_identity,
+	include = c(location, midwest_states,
+							gender, age, income, education),
+	type = list(midwest_states ~ "categorical"))
 
 
